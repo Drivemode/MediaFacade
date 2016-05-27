aa6b533944ac2630bd8ebbf40218849e23172f41
KeithYokoma <Keith.Yokoma@gmail.com>
2016-05-27 15:54:20 +0900

    Legal

--- app/src/main/java/com/drivemode/media/sample/AboutFragment.java
+++ app/src/main/java/com/drivemode/media/sample/AboutFragment.java
@@ -1,10 +1,19 @@
 package com.drivemode.media.sample;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * @author KeishinYokomaku
  */
 public class AboutFragment extends Fragment {
-	public static final String TAG = AboutFragment.class.getSimpleName();
+	@Nullable
+	@Override
+	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_about, container, false);
+	}
 }
